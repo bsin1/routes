155f815d4117745aa99835b2b83bbbfcdc8f262a
Balin Sinnott <sinnott.balin@gmail.com>
2021-10-18 12:10:40 -0600

    updates.

--- src/react/components/Map/index.tsx
+++ src/react/components/Map/index.tsx
@@ -2,9 +2,12 @@ import { useEffect } from "react"
 import styles from "src/react/styles/MapboxMap.module.css"
 import mapboxgl from "mapbox-gl"
 import MapboxDraw from "@mapbox/mapbox-gl-draw"
+import * as turf from "@turf/turf"
 
 const MapboxMap = () => {
-  let map: mapboxgl.Map | null = null
+  let map: mapboxgl.Map
+  let markers: mapboxgl.Marker[] = []
+  let draw: MapboxDraw
   const defaultLat = 0.04092881639623261
   const defaultLng = 0.08654556598968949
   const defaultZoom = 12.5
@@ -1197,31 +1200,69 @@ const MapboxMap = () => {
   ]
 
   useEffect(() => {
+    console.log("USE EFFECT CALLED")
     loadMap()
   })
 
   const loadMap = () => {
+    console.log("LOAD MAP")
+
     let token = process.env.REACT_APP_MAPBOX_TOKEN ?? ""
     console.log("GOT TOKEN: ", token)
     mapboxgl.accessToken = token
     let container = document.getElementById("main-map")!
 
-    let newMap = new mapboxgl.Map({
+    map = new mapboxgl.Map({
       container: container,
       style: "mapbox://styles/bsin1/ckuqizsi81ony14obdblcukec",
       preserveDrawingBuffer: true,
     })
 
-    // let draw = new MapboxDraw()
+    draw = new MapboxDraw()
 
-    // newMap.addControl(draw)
+    map.addControl(draw)
 
-    newMap.on("load", loadSources)
+    map.on("draw.create", updateArea)
+    // map.on("draw.delete", updateArea)
+    // map.on("draw.update", updateArea)
 
-    map = newMap
+    map.on("load", loadSources)
+  }
+
+  const updateArea = (event: any) => {
+    console.log("GOT DRAW EVENT: ", event)
+
+    let polygon = event.features[0]
+    let selectedMarkers: any[] = []
+
+    //create bounding box from polygon
+    let polygonBoundingBox = turf.bbox(polygon)
+    let southWest = map.project([polygonBoundingBox[0], polygonBoundingBox[1]])
+    let northEast = map.project([polygonBoundingBox[2], polygonBoundingBox[3]])
+
+    //find features within polygon
+    // let bboxFeatures: any[] = map.queryRenderedFeatures(
+    //   [southWest, northEast]
+    //   // {
+    //   //   layers: ["choropleth"], //Update to use layers in user's map, tested with circle and choropleth
+    //   // }
+    // )
+
+    markers.forEach((marker) => {
+      let lnglat = marker.getLngLat()
+      if (turf.booleanContains(polygon, turf.point([lnglat.lng, lnglat.lat]))) {
+        selectedMarkers.push(marker)
+      }
+    })
+
+    console.log("MARKERS SIZE: ", markers.length)
+
+    console.log("SELECTED MARKERS SIZE: ", selectedMarkers.length)
+    console.log("SELECTED MARKERS: ", selectedMarkers)
   }
 
   const loadMarkers = () => {
+    console.log("LOAD MARKERS")
     if (!map) {
       return
     }
@@ -1230,29 +1271,38 @@ const MapboxMap = () => {
       .setLngLat(new mapboxgl.LngLat(city.x, city.y))
       .addTo(map!)
 
-    ores.forEach((ore) => {
-      let marker = new mapboxgl.Marker({ color: "#ff0000" })
-        .setLngLat(new mapboxgl.LngLat(ore.x, ore.y))
-        .addTo(map!)
+    markers.push(marker)
 
-      let markerDiv = marker.getElement()
+    ores.forEach((ore, i) => {
+      if (i < 50) {
+        let marker = new mapboxgl.Marker({ color: "#ff0000" })
+          .setLngLat(new mapboxgl.LngLat(ore.x, ore.y))
+          .addTo(map!)
 
-      markerDiv.addEventListener("click", () =>
-        console.log("CLICKED: ", marker.getLngLat())
-      )
+        let markerDiv = marker.getElement()
+
+        markerDiv.addEventListener("click", () =>
+          console.log("CLICKED: ", marker.getLngLat())
+        )
+
+        markers.push(marker)
+      }
     })
+
+    console.log("ORIGINAL MARKERS SIZE: ", markers.length)
   }
 
   const loadSources = () => {
+    console.log("LOAD SOURCES")
     if (map?.isStyleLoaded() === false) {
+      console.log("STYLE NOT YET LOADED")
       setTimeout(() => {
         loadSources()
       }, 250)
+    } else {
+      loadMarkers()
+      zoomToMapRegion(defaultLat, defaultLng, defaultZoom)
     }
-
-    loadMarkers()
-
-    zoomToMapRegion(defaultLat, defaultLng, defaultZoom)
   }
 
   const zoomToMapRegion = (
